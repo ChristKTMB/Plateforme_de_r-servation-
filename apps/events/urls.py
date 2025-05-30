0fea5731dafc8febd898fa0a6024a478917fb932
BlockDev <bisimwad6@gmail.com>
2025-05-30 01:04:16 +0100

    Mis à jour de apps/events: Meilleur implémentation de la vue et urls

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path, include
 from django.conf.urls.static import static
-from . import views
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
+from . import views
 
+# Configuration du routeur DRF
 router = DefaultRouter()
-router.register(r'events', views.EventViewSet)
+router.register(r'', views.EventViewSet, basename='events')
 
 app_name = 'events'
 
 urlpatterns = [
-    # Routes pour l'API
-    path('api/', include(router.urls)),
-    
-    # Routes pour les templates
-    path('create/', views.create_event, name='create_event'),
-    path('detail/<int:pk>/', views.event_detail, name='event_detail'),
-    path('list/', views.event_list, name='event_list'),
+    path('list/', views.EventListView.as_view(), name='event_list'),
+    path('create/', views.EventCreateView.as_view(), name='event_create'),
+    path('<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+
+    # Routes pour l'API et les templates, gérées par le même ViewSet
+    path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.core.exceptions import PermissionDenied
-from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
@@ -14,112 +14,179 @@ from .permissions import IsOrganizerOrReadOnly
 from .forms import EventForm, TicketTypeFormSet
 from .models import Event
 
-@login_required
-def create_event(request):
-    # Vérifier si l'utilisateur est un organisateur
-    if request.user.user_type != 'O':
-        raise PermissionDenied("Seuls les organisateurs peuvent créer des événements.")
+# @login_required
+# def create_event(request):
+#     # Vérifier si l'utilisateur est un organisateur
+#     if request.user.user_type != 'O':
+#         raise PermissionDenied("Seuls les organisateurs peuvent créer des événements.")
     
-    if request.method == 'POST':
-        form = EventForm(request.POST, request.FILES)
-        formset = TicketTypeFormSet(request.POST)
+#     if request.method == 'POST':
+#         form = EventForm(request.POST, request.FILES)
+#         formset = TicketTypeFormSet(request.POST)
         
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.organizer = request.user
-            event.save()
-            form.save_m2m()
+#         if form.is_valid():
+#             event = form.save(commit=False)
+#             event.organizer = request.user
+#             event.save()
+#             form.save_m2m()
             
-            formset = TicketTypeFormSet(request.POST, instance=event)
-            if formset.is_valid():
-                formset.save()
-                messages.success(request, "L'événement a été créé avec succès!")
-                return redirect('events:event_detail', pk=event.pk)
-            else:
-                event.delete()
-    else:
-        form = EventForm()
-        formset = TicketTypeFormSet()
+#             formset = TicketTypeFormSet(request.POST, instance=event)
+#             if formset.is_valid():
+#                 formset.save()
+#                 messages.success(request, "L'événement a été créé avec succès!")
+#                 return redirect('events:event_detail', pk=event.pk)
+#             else:
+#                 event.delete()
+#     else:
+#         form = EventForm()
+#         formset = TicketTypeFormSet()
     
-    return render(request, 'events/create_event.html', {
-        'form': form,
-        'formset': formset
-    })
+#     return render(request, 'events/create_event.html', {
+#         'form': form,
+#         'formset': formset
+#     })
 
-def event_detail(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-    ticket_types = event.ticket_types.all()
+# def event_detail(request, pk):
+#     event = get_object_or_404(Event, pk=pk)
+#     ticket_types = event.ticket_types.all()
     
-    context = {
-        'event': event,
-        'ticket_types': ticket_types,
-    }
-    return render(request, 'events/event_detail.html', context)
+#     context = {
+#         'event': event,
+#         'ticket_types': ticket_types,
+#     }
+#     return render(request, 'events/event_detail.html', context)
 
-def event_list(request):
-    events = Event.objects.filter(is_published=True).order_by('-date')
+# def event_list(request):
+#     events = Event.objects.filter(is_published=True).order_by('-date')
 
-    paginator = Paginator(events, 10)
-    page = request.GET.get('page')
-    events = paginator.get_page(page)
+#     paginator = Paginator(events, 10)
+#     page = request.GET.get('page')
+#     events = paginator.get_page(page)
 
-    return render(request, 'events/event_list.html', {'events': events})
+#     return render(request, 'events/event_list.html', {'events': events})
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'events/event_list.html'
+    context_object_name = 'events'
+    queryset = Event.objects.filter(is_published=True)
+    paginate_by = 10
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+    context_object_name = 'event'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ticket_types'] = self.object.ticket_types.all()
+        return context
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    template_name = 'events/create_event.html'
+    form_class = EventForm
+    success_url = reverse_lazy('events:event_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = TicketTypeFormSet(self.request.POST)
+        else:
+            context['formset'] = TicketTypeFormSet()
+        return context
+    
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save(commit=False)
+            self.object.organizer = self.request.user
+            self.object.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.filter(is_published=True)
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOrganizerOrReadOnly]
     renderer_classes = [JSONRenderer, TemplateHTMLRenderer]
-    template_name = 'events/event_list.html'
 
-    def list(self, request, *args, **kwargs):
-        if request.accepted_renderer.format == 'html':
-            # Pour le rendu template
-            events = self.get_queryset()
-            paginator = Paginator(events, 10)
-            page = request.GET.get('page')
-            events = paginator.get_page(page)
-            return Response({'events': events})
-        # Pour l'API
-        return super().list(request, *args, **kwargs)
+    def get_template_names(self):
+        """Retourne le template approprié selon l'action"""
+        templates = {
+            'list': 'events/event_list.html',
+            'retrieve': 'events/event_detail.html',
+            'create': 'events/create_event.html',
+        }
+        return [templates.get(self.action, 'events/event_list.html')]
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if request.accepted_renderer.format == 'html':
-            return Response({
-                'event': instance,
-                'ticket_types': instance.ticket_types.all()
-            }, template_name='events/event_detail.html')
-        return super().retrieve(request, *args, **kwargs)
+    def get_renderer_context(self):
+        """Prépare le contexte pour le rendu"""
+        context = super().get_renderer_context()
+        if self.request.accepted_renderer.format == 'html':
+            if self.action == 'create':
+                context.update({
+                    'form': EventForm(),
+                    'formset': TicketTypeFormSet()
+                })
+            elif self.action == 'list':
+                events = self.paginate_queryset(self.get_queryset())
+                context['events'] = events
+            elif self.action == 'retrieve':
+                context.update({
+                    'event': self.get_object(),
+                    'ticket_types': self.get_object().ticket_types.all()
+                })
+        return context
 
     def create(self, request, *args, **kwargs):
         if request.accepted_renderer.format == 'html':
-            if request.method == 'GET':
-                form = EventForm()
-                formset = TicketTypeFormSet()
-                return Response({
-                    'form': form,
-                    'formset': formset
-                }, template_name='events/create_event.html')
-            
-            form = EventForm(request.POST, request.FILES)
-            formset = TicketTypeFormSet(request.POST)
-            if form.is_valid() and formset.is_valid():
-                event = form.save(commit=False)
-                event.organizer = request.user
-                event.save()
-                form.save_m2m()
-                formset.instance = event
-                formset.save()
-                return redirect('events:event_detail', pk=event.pk)
-            return Response({
-                'form': form,
-                'formset': formset
-            }, template_name='events/create_event.html')
-        return super().create(request, *args, **kwargs)
+            return self._handle_html_create(request)
+        return self._handle_api_create(request)
+
+    def _handle_html_create(self, request):
+        """Gestion de la création via formulaire HTML"""
+        if request.method == 'GET':
+            return Response(self.get_renderer_context())
+        
+        form = EventForm(request.POST, request.FILES)
+        formset = TicketTypeFormSet(request.POST)
+        
+        if form.is_valid() and formset.is_valid():
+            event = self._save_event(form, formset)
+            messages.success(request, "L'événement a été créé avec succès!")
+            return redirect('events:event_detail', pk=event.pk)
+        
+        return Response({'form': form, 'formset': formset})
+
+    def _save_event(self, form, formset):
+        """Sauvegarde l'événement et ses tickets"""
+        event = form.save(commit=False)
+        event.organizer = self.request.user
+        event.save()
+        form.save_m2m()
+        formset.instance = event
+        formset.save()
+        return event
+
+    def _handle_api_create(self, request):
+        """Gestion de la création via l'API"""
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        """Personnalisation de la création d'un événement"""
+        serializer.save(organizer=self.request.user)
 
     @action(detail=True, methods=['post'])
     def add_ticket_type(self, request, pk=None):
+        """Ajoute un type de ticket à un événement"""
         event = self.get_object()
         serializer = TicketTypeSerializer(data=request.data)
         
